dbf54cd2e44826a2af0da08024545a3953b9efda
Lekha P N <lekhapn@in.ibm.com>
2021-05-28 13:03:13 +0530

    Modified abc and xyz and added atoz

---
+++ AtoZ.java
@@ -0,0 +1,6 @@
+public class AtoZ
+{
+	public static void main(String args[])
+	{
+	}
+}
--- Xyz.java
+++ Xyz.java
@@ -3,6 +3,7 @@ public class Xyz
 	publlic void 
 	{
 		System.out.println("From Xyz Class");
+		System.out.println("By lekha");
 	}
 
 }
